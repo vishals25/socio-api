f0d90e4f332daae6bf0a88b489ddb80b3655dfc8
vishal_s_02 <svs15324@gmail.com>
2024-07-30 20:57:48 +0530

    include-pydantic

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
 
 
-# Path Operation
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating:Optional[int]=None
+
+# Path Operation 
 
 @app.get("/")
-async def root():
-    return ["message","Hello World"]
+def root():
+    return "Hello World"
+
+
+@app.get("/posts")
+def get_posts():
+    return [{"title": "Post 1", "content": "Content of post 1"},
+            {"title": "Post 2", "content": "Content of post 2"}]
+
+
+@app.post("/createposts")
+def create_posts(post:Post):
+
+    # title -> str,# content -> str
+    print(post.model_dump())
+    return {"post": f"{post}"}
