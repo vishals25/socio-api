47d7078936bab33f9d6413997fcd617d92c7269e
vishal_s_02 <svs15324@gmail.com>
2024-08-13 14:24:38 +0530

    CORS-implementation-01

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from .routers import post,user,auth,vote
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+origins=["*"] #domain of front end application change it in deployment
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
--- app/routers/post.py
+++ app/routers/post.py
@@ -13,7 +13,7 @@ router=APIRouter(
 
 @router.get("/",response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db),current_user:int=Depends(oauth2.get_current_user),
-              limit:int = 10,skip:int=0,search:Optional[str]=None):
+              limit:int = 10,skip:int=0,search:Optional[str]=""):
 
     # posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     
