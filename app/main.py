6bca1cd83081101830193fc6836c19e5526827a8
vishal_s_02 <svs15324@gmail.com>
2024-08-13 14:57:41 +0530

    heroku-01

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-origins=["*"] #domain of front end application change it in deployment
+origins=["*"] # domain of front end application change it in deployment
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],   
     allow_headers=["*"],
 )
 
